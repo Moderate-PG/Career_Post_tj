580281312cfea500fe9aa75bd0b1a8213f96b55d
Piers Gillett <p.gillett.92@gmail.com>
2019-12-03 08:07:00 +1100

    Added a few comments about plans for accessing final appearance data

--- data_cleaning.R
+++ data_cleaning.R
@@ -16,3 +16,10 @@ tj_list$Team <- as.factor(tj_list$Team)
 # The 'Team' variable assumes that a player started with and stayed with a team throughout rehab.
 # At this point, I am only assessing by the team at time of the surgery although changing teams is certainly going to influence thing
 
+# Import appearance data from pybaseball
+
+# I can't just use playerid_lookup and return everything, last name is required. So I have 2 options as I can see it at
+# the moment: 1) need to run a python loop (either in R Markdown or a python script) going through all the names from 
+# the tj_list and extract the dates from the pybaseball data frame. The issue with this data is that it only has years
+# and not month and day. 
+# Option 2) is to maybe webscrape and get dates but not sure where to webscrape from. 

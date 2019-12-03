507f2e5bcda4f3accd4d6663ec229a32f2122c8f
Piers Gillett <p.gillett.92@gmail.com>
2019-12-03 19:51:12 +1100

    Loaded in a bunch of data from the Lahman database and created the dataset with the list of players having received TJ and their final game.

--- data_cleaning.R
+++ data_cleaning.R
@@ -1,6 +1,7 @@
 # Data cleaning and merging of datasets for tommy john career survival analysis
 
 # Dataset of List of pitchers having received tommy john
+setwd("C:/Users/pgill/Documents/Data Science Portfolio/TJ_survival")
 tj_list <- read_csv("tj_list_mlb.csv")
 tj_list$`TJ Surgery Date` <- mdy(tj_list$`TJ Surgery Date`)
 tj_list <- tj_list %>%
@@ -16,10 +17,26 @@ tj_list$Team <- as.factor(tj_list$Team)
 # The 'Team' variable assumes that a player started with and stayed with a team throughout rehab.
 # At this point, I am only assessing by the team at time of the surgery although changing teams is certainly going to influence thing
 
-# Import appearance data from pybaseball
 
-# I can't just use playerid_lookup and return everything, last name is required. So I have 2 options as I can see it at
-# the moment: 1) need to run a python loop (either in R Markdown or a python script) going through all the names from 
-# the tj_list and extract the dates from the pybaseball data frame. The issue with this data is that it only has years
-# and not month and day. 
-# Option 2) is to maybe webscrape and get dates but not sure where to webscrape from. 
+# Load in the pitching appearance data
+setwd("C:/Users/pgill/Documents/Data Science Portfolio/TJ_survival/Appearance data/baseballdatabank-2019.2/baseballdatabank-2019.2/core/")
+
+people <- read_csv("People.csv") 
+people <- people %>%
+  select(playerID, nameFirst, nameLast, nameGiven, finalGame)
+
+pitchers <-  read_csv("Pitching.csv")
+pitchers <- pitchers %>%
+  select(playerID) %>%
+  arrange(playerID) %>%
+  unique.data.frame()
+
+# Join the pitchers and people tables
+pitcher_list <- inner_join(people, pitchers, "playerID")
+
+pitcher_list$Player <- paste(pitcher_list$nameFirst,  pitcher_list$nameLast)
+
+tj_final_game <- inner_join(tj_list, pitcher_list, "Player") 
+# There are 7 fewer rows here than in tj_list which I don't know why they're missing. That needs to be looked into.
+tj_final_game <- tj_final_game %>%
+  select(Player:Team, Age:playerID, finalGame)
